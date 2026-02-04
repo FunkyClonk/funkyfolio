447fd258e658359d99e1e737003fb06dbbaead17
Lightmare57 <basauffisseau@gmail.com>
2026-02-04 21:37:58 +0100

    Test deployement

--- next.config.js
+++ next.config.js
@@ -4,19 +4,16 @@
  */
 import "./src/env.js";
 
-/** @type {import("next").NextConfig} */
-const config = {
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  images: {
+    unoptimized: true, // Disable default image optimization
   },
+  assetPrefix: isProd ? '/funkyfolio/' : '',
+  basePath: isProd ? '/funkyfolio' : '',
+  output: 'export'
 };
 
-export default config;
+export default nextConfig;
+
