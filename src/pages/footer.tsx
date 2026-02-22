5fc01e13f345e239e528dacb39d4e6c54eab10ba
Lightmare57 <basauffisseau@gmail.com>
2026-02-22 21:34:12 +0100

    Fixing apostr typo

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4 border-white">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between text-white">
-          <span className="text-sm text-body sm:text-center"> Let's work together! <a href="mailto:funkyclonk@proton.me" className="hover:underline">Contact me</a>.
+          <span className="text-sm text-body sm:text-center"> Let&apos;s work together! <a href="mailto:funkyclonk@proton.me" className="hover:underline">Contact me</a>.
         </span>
         </div>
     </footer>
