754772519f557a8fcbbce3a0f361eca1973c70e1
Lightmare57 <basauffisseau@gmail.com>
2026-02-22 21:34:28 +0100

    Fixing apostr typo

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ export default function About() {
     <div id="about">
         <h1 className="text-4xl font-bold text-green-300 mt-10">About me</h1>
         <p className="mt-4 text-4xl text-white"> 
-          I'm an cybersecurity and DevOps enthusiast with a strong passion for problem solving and a keen
+          I&apos;m an cybersecurity and DevOps enthusiast with a strong passion for problem solving and a keen
           interest in the intersection of security and development.
           With a background in cybersecurity, I have honed my skills in identifying vulnerabilities,
           implementing robust security measures, and ensuring the integrity of systems. 
